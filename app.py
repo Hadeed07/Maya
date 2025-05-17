7b0953b983c8565f418d21eb63497fe5f36bbe9a
Hadeed Qamar <145800004+Hadeed07@users.noreply.github.com>
2025-05-17 10:34:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ class Maya:
         return response.text
     
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='Templates')
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'instance', 'database.db')}"
